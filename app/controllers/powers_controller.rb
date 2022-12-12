46df8d4d01ceebff933604922b38184e8815eacd
Juspher Mokua <juspermokua6@gmail.com>
2022-12-12 19:31:18 +0300

    done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,36 @@
 class PowersController < ApplicationController
+        # GET /powers
+  def index
+    powers = Power.all
+    render json: powers
+  end
+
+  #  GET /powers/:id
+  def show
+    power = Power.find(params[:id])
+    render json: power
+  end
+
+  # PATCH /powers/:id
+  def update
+    #find
+    power = Power.find(params[:id])
+    # update
+    power.update!(power_params)
+    render json: power
+  end
+
+  private
+
+  def power_params
+    params.permit(:id, :name, :description)
+  end
+
+  def unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def power_not_found
+    render json: { error: "Power not found" }, status: :not_found
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Start Seeding Heroes here ..."
+Hero.create!(
+  [
+    {
+      name: "Peter Parker",
+      super_name: "Spider-Man",
+    },
+
+    {
+      name: "Steve Rogers",
+      super_name: "Captain America",
+    },
+
+    {
+      name: "Bruce Banner",
+      super_name: "Hulk",
+    },]
+)
+
+Power.create!(
+  [
+    {
+      name: "Spidey ",
+      description: "gives the wielder super strenght and quickness",
+    },
+
+    {
+      name: "Captain Fantastic ",
+      description: "gives the wielder super strenght  ",
+    },
+
+    {
+      name: "Green Beast ",
+      description: "allows the person to shift into a big super stong human",
+    },
+
+  ]
+)
+
+puts "End of Seeding...."
